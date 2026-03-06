0a91eb47ff134f9c6a1f93ab8dbd3f2451231bda
bisnessdrop225-hub <bisnessdrop225@gmail.com>
2026-03-06 18:18:15 +0100

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,1 +1,36 @@
 
+port Navbar from '../components/elite/Navbar';
+import Hero from '../components/elite/Hero';
+import MarqueeBar from '../components/elite/MarqueeBar';
+import EditorialIntro from '../components/elite/EditorialIntro';
+import SmileGallery from '../components/elite/SmileGallery';
+import Services from '../components/elite/Services';
+import Amenities from '../components/elite/Amenities';
+import Team from '../components/elite/Team';
+import Testimonials from '../components/elite/Testimonials';
+import BookingCTA from '../components/elite/BookingCTA';
+import Location from '../components/elite/Location';
+import Footer from '../components/elite/Footer';
+import FloatingWhatsApp from '../components/elite/FloatingWhatsApp';
+
+const CLINIC = 'Elite Dental Gallery';
+
+export default function Home() {
+  return (
+    <div style={{ background: '#FFFFFF', overflowX: 'hidden' }}>
+      <Navbar clinicName={CLINIC} />
+      <Hero clinicName={CLINIC} />
+      <MarqueeBar />
+      <EditorialIntro />
+      <SmileGallery />
+      <Services />
+      <Amenities />
+      <Team clinicName={CLINIC} />
+      <Testimonials />
+      <BookingCTA />
+      <Location />
+      <Footer clinicName={CLINIC} />
+      <FloatingWhatsApp />
+    </div>
+  );
+}
